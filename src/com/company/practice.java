8d6410a75d15e8ae6bc4c6419837ad8dfde310e8
gagandeep kaur <sherry2312.gk@gmail.com>
2020-01-22 00:54:47 -0500

    fghmhg

---
+++ src/com/company/practice.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class practice {
+    public static void main (String[ ]args){
+        System.out.println("hello");
+        System.out.println("gagandeep kaur");
+        System.out.println("my first java class");
+    }
+}
